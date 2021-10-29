c15c0c08d5ee01d477c7626a417ef3f6e41a582d
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-10-29 19:39:49 +0300

    +

--- src/components/App.js
+++ src/components/App.js
@@ -21,23 +21,31 @@ import { v4 as uuidv4 } from "uuid";
 import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
-  const [contacts, setContacts] = useLocalStorage("contacts", "");
+  // const [contacts, setContacts] = useLocalStorage("contacts", '')
+  const [contacts, setContacts] = useLocalStorage("contacts", [
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ]);
   const [filter, setFilter] = useState("");
   // const [showModal, setShowModal] = useState(false);
 
   const addContact = ({ name, number }) => {
-    const person = {
-      id: uuidv4(),
-      name,
-      number,
-    };
+    const person = [
+      {
+        id: uuidv4(),
+        name,
+        number,
+      },
+    ];
 
     if (contacts.some((contact) => contact.name === person.name)) {
       toast.error("This contact is already exist ! Try one more time, please!");
       return;
     }
 
-    setContacts((prevState) => [...prevState, ...person]);
+    setContacts((prevContacts) => [...person, ...prevContacts]);
   };
 
   const deleteContact = (id) => {
